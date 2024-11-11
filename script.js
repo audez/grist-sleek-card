73e965c9fff96de2545a096d21e1a61d2d5f1ec8
audez <28386648+audez@users.noreply.github.com>
2024-11-11 16:02:29 +0100

    a

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 
-const columnsToMap = ['Title', 'Subtitle', 'Image', 'Text1', 'Text2']
-const htmlReferences = ['title', 'subtitle', 'image', 'text1', 'text2']
-  
-grist.ready({columns: columnsToMap, requiredAccess: 'read table'});
+function toggleConfigurationPanel() {
+    $('#slideout').toggleClass('on');
+}
+function initGrist() {
+  const columnsToMap = ['Title', 'Subtitle', 'Image', 'Text1', 'Text2']
+  const htmlReferences = ['title', 'subtitle', 'image', 'text1', 'text2']
 
-grist.onRecord(async (record, mappings) => {
-  console.log("record = " + JSON.stringify(record))
+  grist.ready({columns: columnsToMap, requiredAccess: 'read table'});
 
-  
-  const mapped = grist.mapColumnNames(record);
-  console.log("mapped = " + JSON.stringify(mapped))
-    
-  // First check if all columns were mapped by user
-  if (Object.keys(mapped).length !== 0) {
-    // Map columns and html elements  
-    columnsToMap.forEach(function (item, index) {      
-      document.getElementById(htmlReferences[index]).innerText = mapped[item];
-    });
-    
-    //getting img 
-    const tokenInfo = await grist.docApi.getAccessToken({readOnly: true});
-    const id = mapped.Image[0];  // only get the first attachment (there could be several)
-    const src = `${tokenInfo.baseUrl}/attachments/${id}/download?auth=${tokenInfo.token}`;
-    document.getElementById('image').setAttribute('src', src);
-    
-  } else {
-    // Not all required columns were mapped.
-    console.error("Please map at least one column");
-  }
-});
+  grist.onRecord(async (record, mappings) => {
+    console.log("record = " + JSON.stringify(record))
 
 
-$("button").click(function(){
-  $('#slideout').toggleClass('on');
-});
+    const mapped = grist.mapColumnNames(record);
+    console.log("mapped = " + JSON.stringify(mapped))
+
+    // First check if all columns were mapped by user
+    if (Object.keys(mapped).length !== 0) {
+      // Map columns and html elements  
+      columnsToMap.forEach(function (item, index) {
+        document.getElementById(htmlReferences[index]).innerText = mapped[item];
+      });
+
+      //getting img 
+      const tokenInfo = await grist.docApi.getAccessToken({readOnly: true});
+      const id = mapped.Image[0];  // only get the first attachment (there could be several)
+      const src = `${tokenInfo.baseUrl}/attachments/${id}/download?auth=${tokenInfo.token}`;
+      document.getElementById('image').setAttribute('src', src);
+
+    } else {
+      // Not all required columns were mapped.
+      console.error("Please map at least one column");
+    }
+  });
+}
+
+
