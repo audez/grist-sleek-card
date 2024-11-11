48a4c0391cb01353597294fb58529e5519509d7e
audez <maluhia@ikmail.com>
2024-11-11 14:00:08 +0100

    a

---
+++ script.js
@@ -0,0 +1,33 @@
+const columnsToMap = ['Title', 'Subtitle', 'Image', 'Text1', 'Text2']
+const htmlReferences = ['title', 'subtitle', 'image', 'text1', 'text2']
+
+grist.ready(
+    {
+        columns: columnsToMap,
+        requiredAccess: 'read table'
+    }
+);
+
+grist.onRecord(async (record, mappings) => {
+    console.log("record = " + JSON.stringify(record))
+    
+    const mapped = grist.mapColumnNames(record);
+    console.log("mapped = " + JSON.stringify(mapped))
+
+    if (mapped) {
+        // Map columns and html elements  
+        columnsToMap.forEach(function (item, index) {
+            document.getElementById(htmlReferences[index]).innerText = mapped[item];
+        });
+
+        //getting img 
+        const tokenInfo = await grist.docApi.getAccessToken({readOnly: true});
+        const id = mapped.Image[0];  // only get the first attachment (there could be several)
+        const src = `${tokenInfo.baseUrl}/attachments/${id}/download?auth=${tokenInfo.token}`;
+        document.getElementById('image').setAttribute('src', src);
+
+    } else {
+        // Not all required columns were mapped.
+        console.error("Please map at least one column");
+    }
+});
