7ff608ea4804b0ad4f7e8b31a67cf1b1f6125904
audez <28386648+audez@users.noreply.github.com>
2024-11-12 23:12:57 +0100

    a

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function openTab(tabName) {
     var i, tabcontent, tablinks;
 
@@ -77,54 +76,3 @@ grist.onRecord(async (record, mappings) => {
         console.error("Please map at least one column");
     }
 });
-
-
-function changeFont() {
-    const fonts = document.getElementById("fontList");
-    var selectedFont = fonts.options[fonts.selectedIndex].text;
-
-    document.getElementById("home").style.fontFamily = selectedFont
-    document.getElementById("fontList").style.fontFamily = selectedFont
-}
-
-
-const columnsToMap = ['Title', 'Subtitle', 'Image', 'Text1', 'Text2']
-const htmlReferences = ['title', 'subtitle', 'image', 'text1', 'text2']
-
-//columns: columnsToMap, 
-grist.ready({requiredAccess: 'read table'});
-
-grist.onRecord(async (record, mappings) => {
-    // Set the first tab ("home") active
-    const homeTab = document.getElementById("home")
-    homeTab.style.display = "block";
-    homeTab.classList.add("active");
-
-
-    console.log("record = " + JSON.stringify(record))
-
-
-    // const mapped = grist.mapColumnNames(record);
-    // console.log("mapped = " + JSON.stringify(mapped))
-
-    // First check if all columns were mapped by user
-    if (Object.keys(mapped).length !== 0) {
-        // Map columns and html elements  
-        columnsToMap.forEach(function (item, index) {
-            document.getElementById(htmlReferences[index]).innerText = mapped[item];
-        });
-
-        //getting img 
-        const tokenInfo = await grist.docApi.getAccessToken({readOnly: true});
-        const id = mapped.Image[0];  // only get the first attachment (there could be several)
-        const src = `${tokenInfo.baseUrl}/attachments/${id}/download?auth=${tokenInfo.token}`;
-        document.getElementById('image').setAttribute('src', src);
-
-    } else {
-        // Not all required columns were mapped.
-        console.error("Please map at least one column");
-    }
-});
-
-
-
